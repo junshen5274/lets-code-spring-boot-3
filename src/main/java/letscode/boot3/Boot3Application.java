fb62ecac0d3749f9a868fc15464719dc94157505
junshen5274 <joe.shen002@gmail.com>
2023-04-28 17:12:09 -0400

    Bring back the spring-boot-starter-web and spring-boot-starter-actuator. Implement two more event listener bean methods.

--- src/main/java/letscode/boot3/Boot3Application.java
+++ src/main/java/letscode/boot3/Boot3Application.java
@@ -3,7 +3,9 @@ package letscode.boot3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,6 +26,28 @@ public class Boot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot3Application.class, args);
+    }
+
+    @Bean
+    ApplicationListener<WebServerInitializedEvent> webServerInitializedEventApplicationListener() {
+        return new ApplicationListener<WebServerInitializedEvent>() {
+            @Override
+            public void onApplicationEvent(WebServerInitializedEvent event) {
+                log.info("The web server is ready to serve HTTP traffic on port {}.",
+                        event.getWebServer().getPort());
+            }
+        };
+    }
+
+    @Bean
+    ApplicationListener<AvailabilityChangeEvent<?>> availabilityChangeEventApplicationListener() {
+        return new ApplicationListener<AvailabilityChangeEvent<?>>() {
+            @Override
+            public void onApplicationEvent(AvailabilityChangeEvent<?> event) {
+                log.info("The service is healthy? {}",
+                        event.getState().toString());
+            }
+        };
     }
 
     @Bean

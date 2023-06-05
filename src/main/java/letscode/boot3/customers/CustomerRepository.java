4017ed8ac9b7129bcc014eb2a0716ae0bc47752a
junshen5274 <joe.shen002@gmail.com>
2023-06-05 16:01:49 -0400

    Use Spring Data Jdbc

---
+++ src/main/java/letscode/boot3/customers/CustomerRepository.java
@@ -0,0 +1,7 @@
+package letscode.boot3.customers;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+}
--- src/main/java/letscode/boot3/customers/CustomerService.java
+++ src/main/java/letscode/boot3/customers/CustomerService.java
@@ -2,65 +2,38 @@ package letscode.boot3.customers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
 
 @Slf4j
 @Service
 @Transactional
 class CustomerService {
     private final ApplicationEventPublisher publisher;
-    private final JdbcTemplate template;
-    private final RowMapper<Customer> customerRowMapper =
-            (rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("name"), rs.getBoolean("subscribed"));
+    private final CustomerRepository customerRepository;
 
-    CustomerService(JdbcTemplate template, ApplicationEventPublisher publisher) {
-        this.template = template;
+    CustomerService(ApplicationEventPublisher publisher, CustomerRepository customerRowMapper) {
+        this.customerRepository = customerRowMapper;
         this.publisher = publisher;
     }
 
     public Customer add(String name, boolean subscribed) {
-        var al = new ArrayList<Map<String, Object>>();
-        al.add(Map.of("id", Long.class));
-        var keyHolder = new GeneratedKeyHolder(al);
-        template.update(
-                con -> {
-                    var ps = con.prepareStatement("""
-                                    insert into customers (name, subscribed) values (?, ?)
-                                    on conflict on constraint customers_name_key do update set 
-                                    name = excluded.name,
-                              subscribed = excluded.subscribed
-                                    """,
-                            Statement.RETURN_GENERATED_KEYS);
-                    ps.setString(1, name);
-                    ps.setBoolean(2, subscribed);
-                    return ps;
-                },
-                keyHolder
-        );
-        var generatedId = keyHolder.getKeys().get("id");
-        log.info("generatedId: {}", generatedId.toString());
-        Assert.state(generatedId instanceof Number, "The generatedId must be a Number!");
-        Customer customer = findCustomerById(((Number) generatedId).intValue());
-        this.publisher.publishEvent(new CustomerCreatedEvent(customer));
-        return customer;
+        var saved = this.customerRepository.save(new Customer(null, name, subscribed));
+        this.publisher.publishEvent(new CustomerCreatedEvent(saved));
+        return saved;
     }
 
     public Customer findCustomerById(Integer id) {
-        return template.queryForObject(
-                "select id, name, subscribed from customers where id = ?", customerRowMapper, id);
+        return this.customerRepository.findById(id).orElse(null);
     }
 
     public Collection<Customer> all() {
-        return template.query("select * from customers", this.customerRowMapper);
+        var iterable = this.customerRepository.findAll();
+        var list = new ArrayList<Customer>();
+        iterable.forEach(c -> list.add(c));
+        return list;
     }
 }

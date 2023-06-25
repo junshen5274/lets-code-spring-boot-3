8fe18d503175ee239a62e2a7eb609a86186ed99d
junshen5274 <joe.shen002@gmail.com>
2023-06-24 20:54:25 -0400

    start graphql and how to access it from the browser

--- src/main/java/letscode/boot3/customers/HelloWorldGraphqlController.java
+++ src/main/java/letscode/boot3/customers/HelloWorldGraphqlController.java
@@ -9,6 +9,19 @@ public class HelloWorldGraphqlController {
     // A controller has nothing to do with the web, it is just a way to adapt the protocol to the business logic
     // The protocol can be http or socket...
 
+    // Once server is started, run http://localhost:8080/graphiql?path=/graphql in the browser.
+    // In the body enter:
+    // query {
+    //   hello (name: "Joe")
+    // }
+    //
+    // Then we will get the results:
+    // {
+    //   "data": {
+    //     "hello": "Hello, Joe!"
+    //   }
+    // }
+
     @SchemaMapping(typeName = "Query", field = "hello")
     String hello(@Argument String name) {
         return "Hello, " + name + "!";
